39ca492df3cb1a289cadd3d28fe85b4137e25f42
Andreas <amikal@hotmail.no>
2018-09-05 13:10:48 +0200

    haha

--- app/src/main/java/no/hiof/andremi/topptur/tester.java
+++ app/src/main/java/no/hiof/andremi/topptur/tester.java
@@ -1,6 +1,15 @@
 package no.hiof.andremi.topptur;
 
-public class tester {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+public class tester extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        System.out.println("HALLA");
+    }
 }
